37c5d74954b05beefcbb530d23b33357c2c72996
mohith18-bit <mohith18@gmail.com>
2026-01-04 20:45:58 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,7 @@ from .apikeys import router as apikey_router
 from .apikey_manage import router as apikey_manage_router
 from .customer import router as customer_router
 from .usage import router as usage_router
+from .stripe_routes import router as stripe_router
 
 app = FastAPI(title="KeyGen SaaS")
 
@@ -21,6 +22,7 @@ app.include_router(apikey_router)
 app.include_router(apikey_manage_router)
 app.include_router(customer_router)
 app.include_router(usage_router)
+app.include_router(stripe_router)
 
 @app.get("/")
 def home():
