deff713d6e1df9f507f462230143ed61bcf82463
mohith18-bit <mohith18@gmail.com>
2026-01-04 19:22:19 +0530

    Create seed_plans.py

---
+++ backend/app/seed_plans.py
@@ -0,0 +1,15 @@
+from .database import SessionLocal
+from .models import Plan
+
+def seed_plans():
+    db = SessionLocal()
+
+    if db.query(Plan).count() == 0:
+        db.add_all([
+            Plan(name="free", quota_limit=10),
+            Plan(name="pro", quota_limit=10_000),
+            Plan(name="enterprise", quota_limit=1_000_000)
+        ])
+        db.commit()
+
+    db.close()
